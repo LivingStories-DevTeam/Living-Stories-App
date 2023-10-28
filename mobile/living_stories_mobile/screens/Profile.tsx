ea97f0a6dc263c01b8e02d9d0a511dcfe5b7ea6a
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-10-29 00:49:12 +0300

    Profile Design Fix. #49

--- mobile/living_stories_mobile/screens/Profile.tsx
+++ mobile/living_stories_mobile/screens/Profile.tsx
@@ -89,7 +89,7 @@ const Profile = ({ route, navigation }: any) => {
       width: 150,
       height: 150,
       borderRadius: 75,
-      marginBottom: 5,
+      margin: 35,
     },
     title: {
       fontSize: 35,
