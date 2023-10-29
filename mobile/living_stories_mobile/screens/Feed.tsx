e4e46cd669b4e000c677c9af08b0a1bff03f51fa
sananeminli <senan97@gmail.com>
2023-10-30 02:07:52 +0300

    Getting stable build.

--- mobile/living_stories_mobile/screens/Feed.tsx
+++ mobile/living_stories_mobile/screens/Feed.tsx
@@ -19,10 +19,12 @@ import LottieView from "lottie-react-native";
 const Feed = ({ navigation }: any) => {
   const { onLogout } = useAuth();
   const [responseData, setResponseData] = useState(null);
+  const [responseFollowersData, setFollowersResponseData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     try {
+      setIsLoading(true)
       const response = await axios.get(`${API_URL}/stories`);
       setResponseData(response.data);
       setIsLoading(false);
@@ -32,20 +34,35 @@ const Feed = ({ navigation }: any) => {
       setIsLoading(false);
     }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const handleCardPress = (storyId: number) => {
-    navigation.navigate("Story", { storyId });
+  const fetchFollowerData = async () => {
+    try {
+      setIsLoading(true)
+      const response = await axios.get(`${API_URL}/stories/following`);
+      setFollowersResponseData(response.data);
+      
+      console.log(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Request failed:", error);
+      setIsLoading(false);
+    }
   };
-
+  
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   const handleTabChange = (index: number) => {
     setSelectedIndex(index);
   };
+
+  useEffect(() => {
+    if(selectedIndex ===0){fetchData()};
+    if(selectedIndex===1) {fetchFollowerData()}
+  }, [selectedIndex]);
+
+  const handleCardPress = (storyId: number) => {
+    navigation.navigate("Story", { storyId });
+  };
+
 
   return (
     <ScrollView>
@@ -55,11 +72,11 @@ const Feed = ({ navigation }: any) => {
             <View style={styles.animationContainer}>
               <LottieView
                 style={styles.animation}
-                source={require("./globePin.json")}
+                source={require("./globe.json")}
                 autoPlay
                 loop
               />
-              <Text style={{ marginTop: 20 }}>Loading Feed...</Text>
+              <Text style={{ marginTop: 60,  fontSize:20}}>Loading Feed...</Text>
             </View>
           </>
         ) : responseData ?(
@@ -73,27 +90,8 @@ const Feed = ({ navigation }: any) => {
             </View>
             {selectedIndex === 0 && (
               <>
-                <Card
-                  title="The Removal of Lenin's Statu from the center of Baku"
-                  date="29.11.1992 - 2000"
-                  location="Azerbaijan, Turkey"
-                  avatar="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1698542998~exp=1698543598~hmac=f1dde6bce65fc668784143b9e47139ffd1813927888979fa849950d62a7088fd"
-                  labels={[
-                    "memories",
-                    "family",
-                    "happy",
-                    "life",
-                    "fun",
-                    "childhood",
-                    "test",
-                    "story",
-                    "sea",
-                    "love",
-                  ]}
-                  name="Rauf Eminov"
-                  likes={2}
-                  comments={5}
-                />
+                
+                
                 {responseData &&
                   responseData.map((item: any, index: any) => (
                     <>
@@ -134,28 +132,43 @@ const Feed = ({ navigation }: any) => {
             {/* Following Tab */}
             {selectedIndex === 1 && (
               <>
-                <Card
-                  title="The Removal of Lenin's Statu from the center of Baku"
-                  date="29.11.1992 - 2000"
-                  location="Azerbaijan, Turkey"
-                  avatar="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1698542998~exp=1698543598~hmac=f1dde6bce65fc668784143b9e47139ffd1813927888979fa849950d62a7088fd"
-                  labels={[
-                    "memories",
-                    "family",
-                    "happy",
-                    "life",
-                    "fun",
-                    "childhood",
-                    "test",
-                    "story",
-                    "sea",
-                    "love",
-                  ]}
-                  name="Rauf Eminov"
-                  likes={2}
-                  comments={4}
-                />
+                {responseFollowersData &&
+                  responseFollowersData.map((item: any, index: any) => (
+                    <>
+                      <TouchableOpacity
+                        onPress={() => handleCardPress(item.id)}
+                      >
+                        <Card
+                          key={index}
+                          title={item.header}
+                          date={
+                            item.decade
+                              ? item.decade
+                              : item.endDate
+                              ? item.startDate + " - " + item.endDate
+                              : item.startDate
+                          }
+                          location={
+                            item.locations[0].city +
+                            ", " +
+                            item.locations[0].country
+                          }
+                          avatar={
+                            item.user.photo
+                              ? item.user.photo
+                              : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1698542998~exp=1698543598~hmac=f1dde6bce65fc668784143b9e47139ffd1813927888979fa849950d62a7088fd"
+                          }
+                          labels={item.labels}
+                          name={item.user.name}
+                          likes={item.likes.length}
+                          comments={item.comments.length}
+                        />
+                      </TouchableOpacity>
+                    </>
+                  ))}
               </>
+
+              
             )}
           </>
         ): (
