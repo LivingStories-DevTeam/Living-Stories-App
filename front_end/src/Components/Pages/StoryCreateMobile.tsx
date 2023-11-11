be3ff483f6da6d50ba547cdc9f446dade0eacadc
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-11-11 23:22:29 +0300

    Fix

--- front_end/src/Components/Pages/StoryCreateMobile.tsx
+++ front_end/src/Components/Pages/StoryCreateMobile.tsx
@@ -368,7 +368,7 @@ const StoryCreateMobile: React.FC = () => {
   ];
 
   useEffect(() => {
-    /*
+    
     const cookieValue = document.cookie
       .split("; ")
       .find((row) => row.startsWith("jwt_Token"))
@@ -377,7 +377,7 @@ const StoryCreateMobile: React.FC = () => {
     if (!cookieValue) {
       navigate("/login");
     }
-  */
+  
   }, []);
   return (
     <div style={{ backgroundColor: "#1f6c5c" }}>
