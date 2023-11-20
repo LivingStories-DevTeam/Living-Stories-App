b39e767c2d7b032a870e07a728f03df38e9d6d03
JohnsL-U <uzun.cansel@gmail.com>
2023-11-20 18:32:14 +0300

    Added necessary code to request and send data to backend, validate the recieved data and convert it into dataframe to be processed.

--- recommendation_engine/rec_engineV1.py
+++ recommendation_engine/rec_engineV1.py
@@ -1,7 +1,35 @@
+from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial import distance
+import functools
 
+app = Flask(__name__)
+
+#Load the data IF we choose to send the backend database periodically to rec-engines local database
+#data = pd.read_csv('dataset.csv')
+
+def validate_data(data):
+    required_columns = {'user_id', 'story_id', 'story_labels', 'story_latitude', 'story_longitude'}
+    if not all(column in data.columns for column in required_columns):
+        return False, "Missing required columns"
+    return True, "Data is valid"
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    json_data = request.get_json()
+    try:
+        data = pd.DataFrame(json_data)
+        is_valid, message = validate_data(data)
+        if not is_valid:
+            return jsonify({"error": message}), 400
+        global dataset
+        dataset = data
+        return jsonify({"message": "Data received successfully"}), 200
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+@functools.lru_cache(maxsize=100)
 def compute_label_similarity(data):
     user_label_matrix = data.groupby(['user_id', 'story_labels']).size().unstack(fill_value=0)
     return cosine_similarity(user_label_matrix)
@@ -14,16 +42,14 @@ def compute_location_similarity(user_id, data):
         distances.append(distance.euclidean(user_coords, story_coords))
     return distances
 
-
+@functools.lru_cache(maxsize=100)
 def hybrid_recommendations(user_id, data, top_n=5):
     # Compute label and location similarities
     label_similarity = compute_label_similarity(data)
     location_distances = compute_location_similarity(user_id, data)
 
-    # Get stories liked by the user
     liked_stories = data[data['user_id'] == user_id]['story_id'].unique()
-
-    # Get users followed by the user
+    
     followed_users = data[data['user_id'] == user_id]['followed_users'].unique()
 
     scores = {}
@@ -49,8 +75,21 @@ def hybrid_recommendations(user_id, data, top_n=5):
     recommended_stories = sorted(scores, key=scores.get, reverse=True)[:top_n]
     return recommended_stories
 
+def get_recommendations(user_id):
+    # Assuming user_id is an integer. Convert if necessary.
+    user_id = int(user_id)
+    # Call the hybrid_recommendations function with the global 'data'
+    return hybrid_recommendations(user_id, data, top_n=5)
 
-user_id = 123
-recommendations = hybrid_recommendations(user_id, data)
-print(recommendations)
+@app.route('/recommend', methods=['GET'])
+def recommend():
+    user_id = request.args.get('user_id')
+    # Validate user_id is provided and is an integer
+    if user_id is None or not user_id.isdigit():
+        return jsonify({"error": "Invalid or missing user_id"}), 400
+    
+    recommendations = get_recommendations(user_id)
+    return jsonify(recommendations)
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
