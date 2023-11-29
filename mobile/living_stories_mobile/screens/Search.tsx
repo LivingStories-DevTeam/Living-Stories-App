f1a18d46b0a8ede764f698fcbb45f593b7e2c373
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-11-30 01:00:43 +0300

    Search Design Test #96

--- mobile/living_stories_mobile/screens/Search.tsx
+++ mobile/living_stories_mobile/screens/Search.tsx
@@ -7,12 +7,17 @@ import {
   Button,
   TextInput,
   TouchableOpacity,
+  Modal,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { API_URL } from "../contexts/AuthContext";
 import { ScrollView } from "react-native-gesture-handler";
 import Card from "../components/Card";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import MonthSelector from "../components/MonthSelector";
+import moment from "moment";
+import SegmentedControlTab from "react-native-segmented-control-tab";
+import RNPickerSelect from "react-native-picker-select";
 
 interface StoryInt {
   id: number;
@@ -63,8 +68,29 @@ const Search = ({ route, navigation }: any) => {
   const [textStory, setText] = useState<string>();
   const [showPicker, setShowPicker] = useState(false);
   const [date, setDate] = useState(new Date());
+  const [selectedMonth, setSelectedMonth] = useState<moment.Moment | null>(
+    null
+  );
+  const [selectedSeason, setSelectedSeason] = useState(null);
+  const [selectedDecade, setSelectedDecade] = useState(null);
+  const [isModalVisible, setModalVisible] = useState(false);
 
+  ///////////////////////// TABS //////////////////////
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
+  const handleTabChange = (index: number) => {
+    setSelectedIndex(index);
+  };
+  const [selectedSecondIndex, setSelectedSecondIndex] = useState<number>(0);
+
+  const handleSecondTabChange = (index: number) => {
+    setSelectedSecondIndex(index);
+  };
+  const [selectedThirdIndex, setSelectedThirdIndex] = useState<number>(0);
+
+  const handleThirdTabChange = (index: number) => {
+    setSelectedThirdIndex(index);
+  };
   const lat = route.params?.lat;
   const lng = route.params?.lng;
   const radius = route.params?.radius;
@@ -72,6 +98,8 @@ const Search = ({ route, navigation }: any) => {
   const handleCardPress = (storyId: number) => {
     navigation.navigate("Story", { storyId });
   };
+
+  ///////////////////////// TABS END //////////////////////
 
   const showDatepicker = () => {
     setShowPicker(true);
@@ -99,57 +127,291 @@ const Search = ({ route, navigation }: any) => {
       // Handle errors
     }
   };
+  //////////////// Month Selector ///////////
+  const handleMonthTapped = (selectedMonth: moment.Moment) => {
+    console.log("Selected Month:", selectedMonth);
+    setSelectedMonth(selectedMonth);
+    toggleModal();
+  };
 
+  const handleYearChanged = (selectedYear: moment.Moment) => {
+    console.log("Selected Year:", selectedYear);
+  };
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  ////////////////////////////
+
+  const seasons = [
+    { label: "Spring", value: "Spring" },
+    { label: "Summer", value: "Summer" },
+    { label: "Autumn", value: "Autumn" },
+    { label: "Winter", value: "Winter" },
+  ];
+  const decades = [
+    { label: "1900s", value: "1900s" },
+    { label: "1910s", value: "1910s" },
+    { label: "1920s", value: "1920s" },
+    { label: "1930s", value: "1930s" },
+    { label: "1940s", value: "1940s" },
+    { label: "1950s", value: "1950s" },
+    { label: "1960s", value: "1960s" },
+    { label: "1970s", value: "1970s" },
+    { label: "1980s", value: "1980s" },
+    { label: "1990s", value: "1990s" },
+    { label: "2000s", value: "2000s" },
+    { label: "2010s", value: "2010s" },
+    { label: "2020s", value: "2020s" },
+  ];
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <TextInput
-          style={styles.input}
-          placeholder="Text"
-          onChangeText={(text) => setText(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Header"
-          onChangeText={(text) => setHeader(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Label"
-          onChangeText={(text) => setLabel(text)}
-        />
-
-        <TextInput
-          style={styles.input}
-          placeholder="City"
-          onChangeText={(text) => setCity(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Country"
-          onChangeText={(text) => setCountry(text)}
-        />
-      
-      <DateTimePicker
-          value={date}
-          mode="date" // You can also use 'date' or 'time' mode
-          display="default" // 'default', 'spinner', or 'calendar' on Android, 'default' or 'spinner' on iOS
-         
-        />
-    
-
-        <Button
-          title="Select Location"
-          onPress={() => {
-            navigation.replace("SearchMap");
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 15,
+            borderRadius: 10,
+            margin: 10,
           }}
-        ></Button>
-        <Button
-          title="Search"
-          onPress={() => {
-            sendData();
+        >
+          <TextInput
+            style={styles.input}
+            placeholder="Text"
+            onChangeText={(text) => setText(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Header"
+            onChangeText={(text) => setHeader(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Label"
+            onChangeText={(text) => setLabel(text)}
+          />
+
+          <TextInput
+            style={styles.input}
+            placeholder="City"
+            onChangeText={(text) => setCity(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Country"
+            onChangeText={(text) => setCountry(text)}
+          />
+        </View>
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 15,
+            borderRadius: 10,
+            margin: 10,
           }}
-        ></Button>
+        >
+          <View style={{ margin: 5 }}>
+            <SegmentedControlTab
+              values={["Decade", "Date"]}
+              selectedIndex={selectedIndex}
+              onTabPress={handleTabChange}
+            />
+          </View>
+          {selectedIndex === 0 && (
+            <>
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "30%",
+                  marginLeft: "35%", // Adjust this value based on your design
+                  marginRight: "35%", // Adjust this value based on your design
+                }}
+              >
+                <RNPickerSelect
+                  placeholder={{ label: "Select a Decade", value: null }}
+                  items={decades}
+                  onValueChange={(value) => setSelectedDecade(value)}
+                  style={pickerSelectStyles}
+                />
+              </View>
+            </>
+          )}
+          {selectedIndex === 1 && (
+            <>
+              <View style={{ margin: 5 }}>
+                <SegmentedControlTab
+                  values={["Exact Date", "Interval Date"]}
+                  selectedIndex={selectedSecondIndex}
+                  onTabPress={handleSecondTabChange}
+                />
+              </View>
+              {selectedSecondIndex === 0 && (
+                <>
+                  <View
+                    style={{
+                      flex: 1,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "30%",
+                      marginLeft: "35%", // Adjust this value based on your design
+                      marginRight: "35%", // Adjust this value based on your design
+                    }}
+                  >
+                    <RNPickerSelect
+                      placeholder={{ label: "Select a season", value: null }}
+                      items={seasons}
+                      onValueChange={(value) => setSelectedSeason(value)}
+                      style={pickerSelectStyles}
+                    />
+                  </View>
+
+                  <View style={{ margin: 5 }}>
+                    <SegmentedControlTab
+                      values={["Date", "Month", "Year"]}
+                      selectedIndex={selectedThirdIndex}
+                      onTabPress={handleThirdTabChange}
+                    />
+                  </View>
+                  {selectedThirdIndex === 0 && (
+                    <>
+                      <View
+                        style={{
+                          flex: 1,
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <DateTimePicker
+                          value={date}
+                          mode="date" // You can also use 'date' or 'time' mode
+                          display="default" // 'default', 'spinner', or 'calendar' on Android, 'default' or 'spinner' on iOS
+                        />
+                      </View>
+                    </>
+                  )}
+                  {selectedThirdIndex === 1 && (
+                    <>
+                      <View
+                        style={{
+                          flex: 1,
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <TouchableOpacity
+                          onPress={() => {
+                            toggleModal();
+                          }}
+                        >
+                          <Text
+                            style={{
+                              backgroundColor: "#e3e3e3",
+                              padding: 10,
+                              borderRadius: 8,
+                              fontSize: 18,
+                            }}
+                          >
+                            {selectedMonth
+                              ? selectedMonth.format("MMMM YYYY")
+                              : "Select Month"}
+                          </Text>
+                        </TouchableOpacity>
+                      </View>
+                    </>
+                  )}
+                  {selectedThirdIndex === 2 && (
+                    <>
+                      <View
+                        style={{
+                          flex: 1,
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text>Select Year</Text>
+                      </View>
+                    </>
+                  )}
+                </>
+              )}
+            </>
+          )}
+        </View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => {
+            toggleModal();
+          }}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <MonthSelector
+                selectedDate={moment(date)} // Convert to moment object
+                currentDate={moment()} // Convert to moment object
+                minDate={moment("01-01-2000", "DD-MM-YYYY")} // Convert to moment object
+                maxDate={moment()} // Replace with your max date
+                onMonthTapped={handleMonthTapped}
+                onYearChanged={handleYearChanged}
+                selectedBackgroundColor="#000" // Replace with your color
+                selectedMonthTextStyle={{ color: "#fff" }} // Replace with your styles
+                seperatorColor="#b6c3cb" // Replace with your color
+                seperatorHeight={1} // Replace with your height
+                nextText="Next"
+                prevText="Prev"
+                monthFormat="MMM"
+                initialView={moment()} // Convert to moment object
+                monthDisabledStyle={{ color: "#00000050" }} // Replace with your styles
+                localeLanguage="en" // Replace with your desired locale
+                localeSettings={{}} // Replace with your locale settings
+                swipable={true}
+                velocityThreshold={0.3}
+                directionalOffsetThreshold={80}
+                gestureIsClickThreshold={5}
+              />
+
+              <Button
+                title="Close"
+                onPress={() => {
+                  toggleModal();
+                }}
+              />
+            </View>
+          </View>
+        </Modal>
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 15,
+            borderRadius: 10,
+            margin: 10,
+          }}
+        >
+          <Button
+            title="Select Location"
+            onPress={() => {
+              navigation.replace("SearchMap");
+            }}
+          ></Button>
+        </View>
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 15,
+            borderRadius: 10,
+            margin: 10,
+          }}
+        >
+          <Button
+            title="Search"
+            onPress={() => {
+              sendData();
+            }}
+          ></Button>
+        </View>
         <View>
           {stories?.map((item: StoryInt, index: number) => (
             <>
@@ -189,6 +451,7 @@ const Search = ({ route, navigation }: any) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#1f6c5c",
   },
   titleText: {
     fontSize: 24,
@@ -208,6 +471,50 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 1,
     borderColor: "white",
+  },
+  selectedDateContainer: {
+    marginTop: 20,
+    alignItems: "center",
+  },
+  selectedDateText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: "80%", // Adjust the width as needed
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center", // Center the content horizontally
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 10,
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 4,
+    color: "black",
+    paddingRight: 28,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: "purple",
+    borderRadius: 8,
+    color: "black",
+    paddingRight: 30,
   },
 });
 
