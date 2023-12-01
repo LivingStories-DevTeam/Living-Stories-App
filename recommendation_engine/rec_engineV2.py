217f902f8d751c73e6453e2ba9fa1be236e04ee2
JohnsL-U <uzun.cansel@gmail.com>
2023-12-02 01:25:54 +0300

    Updated to fix bugs, data fetching issues. Added code to no recommend  user's own stories.

--- recommendation_engine/rec_engineV2.py
+++ recommendation_engine/rec_engineV2.py
@@ -34,23 +34,12 @@ locations_df = pd.DataFrame({
 
 # Database Functions
 db_config = {
-    'host': 'jdbc:postgresql://db:5432/living_stories',
+    'host': 'localhost',
     'dbname': 'living_stories',
-    'user': 'postgres',
-    'password': 'senan'
+    'user': 'cuzun',
+    'password': '9192',
+    'port': '5432'
 }
-
-def fetch_most_liked_stories(top_m):
-    query = """
-    SELECT stories.*, COUNT(likes.story_id) as like_count
-    FROM stories
-    JOIN likes ON stories.id = likes.story_id
-    GROUP BY stories.id
-    ORDER BY like_count DESC
-    LIMIT %s;
-    """
-    return fetch_data(query, (top_m,))
-
 
 def connect_to_database():
     return psycopg2.connect(**db_config)
@@ -59,89 +48,81 @@ def fetch_data(query):
     with connect_to_database() as conn:
         return pd.read_sql_query(query, conn)
 
-# Fetch functions for each table
 def fetch_users():
     return fetch_data("SELECT * FROM users")
 
 def fetch_stories():
     return fetch_data("SELECT * FROM stories")
 
-def fetch_likes():
-    return fetch_data("SELECT * FROM likes")
-
 def fetch_locations():
     return fetch_data("SELECT * FROM locations")
 
+def fetch_followers():
+    return fetch_data("SELECT * FROM followers")
+
 users_df = fetch_users()
 stories_df = fetch_stories()
-likes_df = fetch_likes()
 locations_df = fetch_locations()
+followers_df = fetch_followers()
 
+def fetch_most_liked_stories(top_m):
+    stories_df['like_count'] = stories_df['likes'].apply(len)
+    most_liked_stories = stories_df.sort_values(by='like_count', ascending=False).head(top_m)
+    return most_liked_stories
 
 def calculate_label_similarity(user_id, stories_df):
-    user_liked_stories = likes_df[likes_df['user_id'] == user_id]['story_id']
+    user_liked_stories = stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)]['id']
     user_labels = set(stories_df[stories_df['id'].isin(user_liked_stories)]['labels'].explode())
-    label_similarity_scores = stories_df['labels'].apply(lambda x: len(set(x).intersection(user_labels)) / len(set(x)))
+    label_similarity_scores = stories_df['labels'].apply(lambda x: len(set(x).intersection(user_labels)) / len(set(x)) if x else 0)
     return label_similarity_scores
 
 def calculate_location_proximity(user_id, stories_df, locations_df):
-    user_liked_stories = likes_df[likes_df['user_id'] == user_id]['story_id']
+    user_liked_stories = stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)]['id']
+    if user_liked_stories.empty:
+        return pd.Series([0]*len(stories_df))
     user_locations = locations_df[locations_df['story_id'].isin(user_liked_stories)][['lat', 'lng']].mean().to_numpy()
     story_locations = locations_df.set_index('story_id').loc[stories_df['id']][['lat', 'lng']].to_numpy()
     proximity_scores = 1 / (1 + np.linalg.norm(story_locations - user_locations, axis=1))
     return proximity_scores
 
 def check_followed_users_likes(user_id, stories_df):
-    followed_users = users_df.loc[user_id - 1, 'followed_users']
-    followed_users_likes = likes_df[likes_df['user_id'].isin(followed_users)]['story_id']
-    followed_user_likes_scores = stories_df['id'].isin(followed_users_likes).astype(int)
+    followed_users = followers_df.loc[followers_df['follower_id'] == user_id, ['following_id']].values[0]
+    #Returns story['id']
+    followed_user_likes = stories_df[stories_df['likes'].apply(lambda likes: any(user in followed_users for user in likes))]['id']
+    followed_user_likes_scores = stories_df['id'].isin(followed_user_likes).astype(int)
     return followed_user_likes_scores
 
-def recommend_stories(user_id, top_n=5):
-    stories_df['label_similarity'] = calculate_label_similarity(user_id, stories_df)
-    stories_df['location_proximity'] = calculate_location_proximity(user_id, stories_df, locations_df)
-    stories_df['followed_user_likes'] = check_followed_users_likes(user_id, stories_df)
+def recommend_stories(user_id, top_n=3):
+    non_user_stories = stories_df[stories_df['user_id'] != user_id]
 
-    # Calculate a combined score
-    stories_df['combined_score'] = stories_df['label_similarity'] + stories_df['location_proximity'] + stories_df['followed_user_likes']
+    non_user_stories['label_similarity'] = calculate_label_similarity(user_id, non_user_stories)
+    non_user_stories['location_proximity'] = calculate_location_proximity(user_id, non_user_stories, locations_df)
+    non_user_stories['followed_user_likes'] = check_followed_users_likes(user_id, non_user_stories)
 
-    # Normalize the scores
-    max_score = stories_df['combined_score'].max()
-    stories_df['normalized_score'] = (stories_df['combined_score'] / max_score) * 10
+    non_user_stories['combined_score'] = non_user_stories['label_similarity'] + non_user_stories['location_proximity'] + non_user_stories['followed_user_likes']
+    max_score = non_user_stories['combined_score'].max()
+    non_user_stories['normalized_score'] = (non_user_stories['combined_score'] / max_score) * 10
 
-    # Get top N recommendations
-    top_recommendations = stories_df.sort_values(by='normalized_score', ascending=False).head(top_n)
+    top_recommendations = non_user_stories.sort_values(by='normalized_score', ascending=False).head(top_n)
     return top_recommendations[['id', 'header', 'normalized_score']]
 
-
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
-    user_id = request.args.get('user_id', type=int)
-    if not user_id:
-        return jsonify({"error": "User ID is required"}), 400
-    
-    recommendations = recommend_stories(user_id)
-    return jsonify(recommendations.to_dict(orient='records'))
-
-@app.route('/recommendations', methods=['GET'])
-def get_recommendations():
+    print(users_df)
     user_id = request.args.get('user_id', type=int)
     if not user_id:
         return jsonify({"error": "User ID is required"}), 400
 
-    cold_start_threshold = 5  
-    top_m_stories = 5  # Top stories to recommend for new users
-    
-    # Check the number of interactions for the user
-    user_interactions = len(likes_df[likes_df['user_id'] == user_id]) + len(users_df.loc[user_id - 1, 'followed_users'])
+    cold_start_threshold = 3
+    top_m_stories = 2
 
+    user_interactions = len(stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)])
     if user_interactions < cold_start_threshold:
         most_liked_stories = fetch_most_liked_stories(top_m_stories)
-        return jsonify(most_liked_stories.to_dict(orient='records'))
+        return jsonify(most_liked_stories[['id', 'header', 'labels']].to_dict(orient='records'))
     else:
         recommendations = recommend_stories(user_id, top_n=5)
         return jsonify(recommendations.to_dict(orient='records'))
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
