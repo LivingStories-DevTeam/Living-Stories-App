6c6e2049c0da46bf3fc995498340dc8bd874ad20
sananeminli <senan97@gmail.com>
2023-12-03 05:58:23 +0300

    Request added for recommendation

--- mobile/living_stories_mobile/screens/Feed.tsx
+++ mobile/living_stories_mobile/screens/Feed.tsx
@@ -10,7 +10,7 @@ import {
   StyleSheet,
 } from "react-native";
 import Card from "../components/Card";
-import { API_URL, useAuth } from "../contexts/AuthContext";
+import { API_URL, RecApiUrl, useAuth } from "../contexts/AuthContext";
 import { ScrollView } from "react-native";
 
 import SegmentedControlTab from "react-native-segmented-control-tab";
@@ -21,7 +21,46 @@ const Feed = ({ navigation }: any) => {
   const { onLogout } = useAuth();
   const [responseData, setResponseData] = useState(null);
   const [responseFollowersData, setFollowersResponseData] = useState(null);
+  const [responseRecomendationData, setRecomendationData] = useState(null);
+
   const [isLoading, setIsLoading] = useState(true);
+
+  const fetchRecommendedStories = async () => {
+    try {
+      const idResponse = await axios.get(`${API_URL}/users/getname`);
+      const idData = idResponse.data;
+      
+      // Extract the id from the response
+      const userId = idData.id;
+      console.log(userId)
+      // Fetch data from the first API endpoint with the custom request name using Axios
+      const firstApiResponse = await axios.get(
+        `${RecApiUrl}/recommendations?user_id=${userId}`
+      );
+      
+
+      const firstApiData = firstApiResponse.data;
+      console.log(firstApiData)
+      // Assuming the response is an array of numbers [1, 3, 4, 5]
+      const numbersArray: number[] = firstApiData;
+
+
+
+      // Send the numberss to the second API endpoint using Axios
+      const secondApiResponse = await axios.post(
+        `${API_URL}/stories/getRecommendedStories`,
+        {
+          StoryID: numbersArray,
+        }
+      );
+      console.log(secondApiResponse.request)
+      const secondApiData = secondApiResponse.data;
+
+      setRecomendationData(secondApiData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   const fetchData = async () => {
     try {
@@ -62,12 +101,16 @@ const Feed = ({ navigation }: any) => {
     if (selectedIndex === 1) {
       fetchFollowerData();
     }
+    if (selectedIndex ===2){
+      fetchRecommendedStories()
+    }
   }, [selectedIndex]);
 
   useFocusEffect(
     useCallback(() => {
       fetchFollowerData();
       fetchData();
+      fetchRecommendedStories()
     }, [])
   );
 
@@ -148,6 +191,44 @@ const Feed = ({ navigation }: any) => {
               <>
                 {responseFollowersData &&
                   responseFollowersData.map((item: any, index: any) => (
+                    <>
+                      <TouchableOpacity
+                        onPress={() => handleCardPress(item.id)}
+                      >
+                        <Card
+                          key={index}
+                          title={item.header}
+                          date={
+                            item.decade
+                              ? item.decade
+                              : item.endDate
+                              ? item.startDate + " - " + item.endDate
+                              : item.startDate
+                          }
+                          location={
+                            item.locations[0].city +
+                            ", " +
+                            item.locations[0].country
+                          }
+                          avatar={
+                            item.user.photo
+                              ? item.user.photo
+                              : "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1698542998~exp=1698543598~hmac=f1dde6bce65fc668784143b9e47139ffd1813927888979fa849950d62a7088fd"
+                          }
+                          labels={item.labels}
+                          name={item.user.name}
+                          likes={item.likes.length}
+                          comments={item.comments.length}
+                        />
+                      </TouchableOpacity>
+                    </>
+                  ))}
+              </>
+            )}
+            {selectedIndex === 2 && (
+              <>
+                {responseRecomendationData &&
+                  responseRecomendationData.map((item: any, index: any) => (
                     <>
                       <TouchableOpacity
                         onPress={() => handleCardPress(item.id)}
