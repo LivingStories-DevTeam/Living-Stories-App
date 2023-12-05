b3b35d4c309a606690b39a975ad589d49a25a3b1
JohnsL-U <uzun.cansel@gmail.com>
2023-12-05 17:56:29 +0300

    Made some naming changes for functions, parameters for code readability. Added user id check to run recommendation engine. Changed the logic for location calculation, now it gives scores based on city and country matches.

--- recommendation_engine/rec_engineV2.py
+++ recommendation_engine/rec_engineV2.py
@@ -79,27 +79,40 @@ locations_df = fetch_locations()
 followers_df = fetch_followers()
 comments_df = fetch_comments()
 
-def fetch_most_liked_stories(top_m):
+def fetch_most_liked_stories(top_l):
     stories_df['like_count'] = stories_df['likes'].apply(len)
-    most_liked_stories = stories_df.sort_values(by='like_count', ascending=False).head(top_m)
+    most_liked_stories = stories_df.sort_values(by='like_count', ascending=False).head(top_l)
     return most_liked_stories
 
 def calculate_label_similarity(user_id, stories_df):
     user_liked_stories = stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)]['id']
     user_labels = set(stories_df[stories_df['id'].isin(user_liked_stories)]['labels'].explode())
-    label_similarity_scores = stories_df['labels'].apply(lambda x: len(set(x).intersection(user_labels)) / len(set(x)) if x else 0)
+    label_similarity_scores = stories_df['labels'].apply(lambda x: len(set(x).intersection(user_labels)) if x else 0)
     return label_similarity_scores
 
-def calculate_location_proximity(user_id, stories_df, locations_df):
-    user_liked_stories = stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)]['id']
-    if user_liked_stories.empty:
-        return pd.Series([0]*len(stories_df))
-    user_locations = locations_df[locations_df['story_id'].isin(user_liked_stories)][['lat', 'lng']].mean().to_numpy()
-    story_locations = locations_df.set_index('story_id').loc[stories_df['id']][['lat', 'lng']].to_numpy()
-    proximity_scores = 1 / (1 + np.linalg.norm(story_locations - user_locations, axis=1))
-    return proximity_scores
+def calculate_location_similarity(user_id, stories_df, locations_df):
+    default_score = 0  
+    user_liked_stories_ids = stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)]['id']
 
-def check_followed_users_likes(user_id, stories_df):
+    if user_liked_stories_ids.empty:
+        return pd.Series([default_score] * len(stories_df), index=stories_df.index)
+
+    user_liked_story_locations = locations_df[locations_df['story_id'].isin(user_liked_stories_ids)]
+    location_similarity_scores = pd.Series([default_score] * len(stories_df), index=stories_df.index)
+
+    for story_id in stories_df['id']:
+        current_story_location = locations_df[locations_df['story_id'] == story_id]
+        if not current_story_location.empty:
+            # Check for any match in city and country
+            match = any((user_liked_story_locations['city'] == current_story_location['city'].values[0]) & 
+                        (user_liked_story_locations['country'] == current_story_location['country'].values[0]))
+            if match:
+                location_similarity_scores.loc[story_id] = 1 
+
+    return location_similarity_scores
+
+
+def calculate_followed_users_likes_scores(user_id, stories_df):
     followed_users = followers_df.loc[followers_df['follower_id'] == user_id, ['following_id']].values[0]
     followed_user_likes = stories_df[stories_df['likes'].apply(lambda likes: any(user in followed_users for user in likes))]['id']
     followed_user_likes_scores = stories_df['id'].isin(followed_user_likes).astype(int)
@@ -113,21 +126,21 @@ def add_like_count(stories_df):
     stories_df['like_count'] = stories_df['likes'].apply(len)
     return stories_df
 
-def recommend_stories(user_id, top_n):
+def recommend_stories(user_id, top_r):
     add_like_count(stories_df)
     X, vectorizer = vectorize_stories(stories_df)
     stories_df['Cluster'] = cluster_stories(X)
     non_user_stories = stories_df[stories_df['user_id'] != user_id]
 
     non_user_stories['label_similarity'] = calculate_label_similarity(user_id, non_user_stories)
-    non_user_stories['location_proximity'] = calculate_location_proximity(user_id, non_user_stories, locations_df)
-    non_user_stories['followed_user_likes'] = check_followed_users_likes(user_id, non_user_stories)
+    non_user_stories['location_similarity'] = calculate_location_similarity(user_id, non_user_stories, locations_df)
+    non_user_stories['followed_user_likes'] = calculate_followed_users_likes_scores(user_id, non_user_stories)
 
-    non_user_stories['combined_score'] = non_user_stories['label_similarity'] + non_user_stories['location_proximity'] + non_user_stories['followed_user_likes']
+    non_user_stories['combined_score'] = non_user_stories['label_similarity'] + non_user_stories['location_similarity'] + non_user_stories['followed_user_likes']
     max_score = non_user_stories['combined_score'].max()
     non_user_stories['normalized_score'] = (non_user_stories['combined_score'] / max_score) * 10
 
-    top_recommendations = non_user_stories.sort_values(by='normalized_score', ascending=False).head(top_n)
+    top_recommendations = non_user_stories.sort_values(by='normalized_score', ascending=False).head(top_r)
     
     columns_to_drop = [col for col in top_recommendations.columns if isinstance(top_recommendations[col].iloc[0], list)]
     top_recommendations = top_recommendations.drop(columns=columns_to_drop)
@@ -141,16 +154,16 @@ def recommend_stories(user_id, top_n):
         clustered_stories = stories_df[stories_df['Cluster'] == user_preferred_cluster]
 
         clustered_stories['cluster_score'] = clustered_stories['like_count'] 
-        top_cluster_stories = clustered_stories.sort_values(by='cluster_score', ascending=False).head(top_n)
+        top_cluster_stories = clustered_stories.sort_values(by='cluster_score', ascending=False).head(top_r)
 
         columns_to_drop = [col for col in top_cluster_stories.columns if isinstance(top_cluster_stories[col].iloc[0], list)]
         top_cluster_stories = top_cluster_stories.drop(columns=columns_to_drop)
 
         # Combine with existing recommendations
-        combined_recommendations = pd.concat([top_recommendations, top_cluster_stories]).head(top_n)
+        combined_recommendations = pd.concat([top_recommendations, top_cluster_stories]).head(top_r)
     
         # Get unique top N recommendations
-        unique_recommendations = combined_recommendations.drop_duplicates(subset='id').head(top_n)
+        unique_recommendations = combined_recommendations.drop_duplicates(subset='id').head(top_r)
         return unique_recommendations['id']
 
     # If there are no preferred cluster, use it without
@@ -158,13 +171,15 @@ def recommend_stories(user_id, top_n):
 
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
-    print(users_df)
     user_id = request.args.get('user_id', type=int)
     if not user_id:
         return jsonify({"error": "User ID is required"}), 400
 
+    if user_id not in users_df['id'].values:
+        return jsonify({"error": "User does not exist"}), 404
+
     cold_start_threshold = 3
-    top_m_stories = 3
+    top_l_stories = 3
 
     user_likes_count = len(stories_df[stories_df['likes'].apply(lambda likes: user_id in likes)])
     followed_users_count = len(followed_users(user_id, stories_df))
@@ -173,10 +188,10 @@ def get_recommendations():
     user_interactions = user_likes_count + followed_users_count + user_comments_count
 
     if user_interactions < cold_start_threshold:
-        most_liked_stories = fetch_most_liked_stories(top_m_stories)
+        most_liked_stories = fetch_most_liked_stories(top_l_stories)
         return jsonify(most_liked_stories['id'].tolist())
     else:
-        recommendations = recommend_stories(user_id, top_n=5)
+        recommendations = recommend_stories(user_id, top_r=3)
         return jsonify(recommendations['id'].tolist())
     
 @app.after_request
