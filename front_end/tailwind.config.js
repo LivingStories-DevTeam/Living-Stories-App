a462b73df300d0e4355806eacddb43d19bb02b79
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-12-11 01:18:50 +0300

    Installed Tailwind

---
+++ front_end/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: "#1f6c5c",
+        customGreenD: "#145245",
+      },
+    },
+  },
+  plugins: [],
+}
