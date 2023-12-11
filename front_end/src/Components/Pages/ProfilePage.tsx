dcc47dbaf1f963f569d48c1475741bfdd0c30d09
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-12-11 05:35:32 +0300

    Redesign Profile Page
    + moved Edit Profile here

--- front_end/src/Components/Pages/ProfilePage.tsx
+++ front_end/src/Components/Pages/ProfilePage.tsx
@@ -1,12 +1,20 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import StoryComponent from "../Components/StoryCard";
+import StoryComponent from "../Components/ProfileStoryCard";
 import NavBar from "../Components/NavBar";
-
+import PeopleIcon from "@mui/icons-material/People";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Card, Col, Row ,Button} from "antd";
+import Upload from "antd/es/upload/Upload";
 import FollowButton from "../Components/Follow";
+import { UploadFile } from "antd";
+import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
+import EditIcon from "@mui/icons-material/Edit";
+import { UploadChangeParam } from "antd/es/upload";
+import { Avatar } from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import SaveAsIcon from "@mui/icons-material/SaveAs";
+import CloseIcon from '@mui/icons-material/Close';
 
 interface Story {
   id: number;
@@ -47,14 +55,24 @@ interface RouteParams {
   [key: string]: string | undefined;
 }
 
+interface EditUserData {
+  biography?: string;
+  photo?: string;
+}
+
 const ProfilePage: React.FC = () => {
   const { name } = useParams<RouteParams>();
   const [user, setUser] = useState<User | null>(null);
   const [isAuthor, setIsAuthor] = useState<boolean>(false);
-  
+  const [bio, setBio] = useState<string>("");
+  const [photo, setPhoto] = useState<string>("");
+  const [trigger, setTrigger] = useState(false);
+  const [editing, setEditing] = useState(false);
+
   const navigate = useNavigate();
-  
-  const token = localStorage.getItem("jwt_Token");  useEffect(() => {
+
+  const token = localStorage.getItem("jwt_Token");
+  useEffect(() => {
     const cookieValue = document.cookie
       .split("; ")
       .find((row) => row.startsWith("jwt_Token"))
@@ -64,7 +82,6 @@ const ProfilePage: React.FC = () => {
       navigate("/login");
     }
   }, []);
-
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -78,6 +95,8 @@ const ProfilePage: React.FC = () => {
           { withCredentials: true }
         );
         setUser(response.data);
+        setPhoto(response.data.photo!);
+        setBio(response.data.biography!);
         if (response_user.data.name === name) {
           setIsAuthor(true);
         }
@@ -87,61 +106,245 @@ const ProfilePage: React.FC = () => {
     };
 
     fetchUser();
-  }, [name]);
+  }, [name,trigger]);
+
+  /////// EDITING
+
+  const handleSubmit = async () => {
+    const editUserData: EditUserData = {
+      photo,
+      biography: bio,
+    };
+    console.log(editUserData);
+
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/users/update`,
+        editUserData,
+        { withCredentials: true }
+      );
+      console.log(response.data);setTrigger(!trigger);setEditing(false);
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+  const handleBioChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBio(event.target.value);
+  };
+
+  const handleImageUpload = (info: UploadChangeParam<UploadFile<any>>) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUrl = reader.result as string;
+      setPhoto(imageDataUrl);
+      console.log(photo);
+    };
+    reader.readAsDataURL(info.file.originFileObj as Blob);
+  };
 
   if (!user) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <NavBar />
-      <Card title="Profile Page">
-        <Row>
-          <Col>
-            {user.photo ? (
-              <Avatar size={150} src={<img src={user.photo} alt="avatar" />} />
+    <>
+      <NavBar />{" "}
+      <div className="p-8 bg-customGreen h-screen">
+        <div className="max-w-3xl mx-auto flex flex-col items-center">
+          <div className="bg-white p-6 rounded-md shadow-md mb-4 w-full md:w-3/4 lg:w-1/2">
+            {editing ? (
+              <>
+                {" "}
+                <div className="flex items-center">
+                  <div className="w-24 h-24 overflow-hidden rounded-full mr-6">
+                    {photo ? (
+                      <>
+                        {" "}
+                        <Avatar
+                          sx={{ width: "100%", height: "100%" }}
+                          src={photo}
+                          className="mr-2"
+                        />
+                      </>
+                    ) : (
+                      <>
+                        {user.photo ? (
+                          <Avatar
+                            sx={{ width: "100%", height: "100%" }}
+                            alt={user?.name}
+                            src={user?.photo}
+                            className="mr-2"
+                          />
+                        ) : (
+                          <Avatar
+                            sx={{
+                              width: "100%",
+                              height: "100%",
+                              backgroundColor: "orange",
+                            }}
+                            className="mr-2"
+                          >
+                            {user?.name[0]}
+                          </Avatar>
+                        )}
+                      </>
+                    )}
+                  </div>
+                  <div>
+                    <h2 className="text-2xl font-semibold">{user.name}</h2>
+                    <div className="flex">
+                      {user.followers && (
+                        <p>
+                          <PeopleIcon /> {user.followers.length}
+                        </p>
+                      )}
+                      {user.stories && (
+                        <p>
+                          <HistoryEduIcon /> {user.stories.length}
+                        </p>
+                      )}
+                    </div>
+                    {isAuthor && (
+                      <div className="flex my-2">
+                        <CloseIcon
+                          className=" cursor-pointer text-gray-600"
+                          onClick={() => setEditing(false)}
+                        />
+                        <p className="text-xs text-gray-700">Cancel</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <Upload
+                  name="avatar"
+                  accept="image/*"
+                  showUploadList={false}
+                  onChange={handleImageUpload}
+                >
+                  <button
+                    type="button"
+                    className="mt-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    <AddPhotoAlternateIcon />
+                    &nbsp; Upload Photo
+                  </button>
+                </Upload>
+                <div className="border border-gray-50 p-2 rounded mt-4">
+                  <textarea
+                    id="message"
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your Bio..."
+                    onChange={handleBioChange}
+                    value={bio}
+                  ></textarea>
+                </div>
+                <button
+                  onClick={handleSubmit}
+                  type="button"
+                  className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                >
+                  <SaveAsIcon />
+                  &nbsp; Save Changes
+                </button>
+              </>
             ) : (
-              <Avatar size={150}>{user?.name}</Avatar>
+              <>
+                {" "}
+                <div className="flex items-center">
+                  <div className="w-24 h-24 overflow-hidden rounded-full mr-6">
+                    {user.photo ? (
+                      <Avatar
+                        sx={{ width: "100%", height: "100%" }}
+                        alt={user?.name}
+                        src={user?.photo}
+                        className="mr-2"
+                      />
+                    ) : (
+                      <Avatar
+                        sx={{
+                          width: "100%",
+                          height: "100%",
+                          backgroundColor: "orange",
+                        }}
+                        className="mr-2"
+                      >
+                        {user?.name[0]}
+                      </Avatar>
+                    )}
+                  </div>
+                  <div>
+                    <h2 className="text-2xl font-semibold">{user.name}</h2>
+                    <div className="flex">
+                      {user.followers && (
+                        <p>
+                          <PeopleIcon /> {user.followers.length}
+                        </p>
+                      )}
+                      {user.stories && (
+                        <p>
+                          <HistoryEduIcon /> {user.stories.length}
+                        </p>
+                      )}
+                    </div>
+                    {isAuthor && (
+                      <div className="flex my-2">
+                        <EditIcon
+                          className=" cursor-pointer text-gray-600"
+                          onClick={() => setEditing(true)}
+                        />
+                        <p className="text-xs text-gray-700">Edit Profile</p>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="border border-gray-50 p-2 rounded mt-4">
+                  <p>{user.biography}</p>
+                </div>
+              </>
             )}
-          </Col>
-          <Col>
-            <p style={{marginLeft:"50px"}}>{user.biography}</p>
-          </Col>
-        </Row>
-        <Row>
-          <h3>{user.name}</h3>
-        </Row>
-        {user.followers && <Row>
-          <p>Number of followers: {user.followers.length}</p>
-        </Row>}
-        {isAuthor ? (
-          <Button
-            onClick={() => {
-              navigate("/edituser");
-            }}
-          >
-            Edit Profile
-          </Button>
-        ) : (
-          <FollowButton followers={user.followers} id={user.id} />
-        )}
-      </Card>
-      <Row style={{marginLeft:"20px"}}>
-        <h2>{user.name}'s Stories </h2>
-      </Row>
-      {user.stories && <Row style={{marginLeft:"20px"}}>
-         <p>{user.name} have {user.stories.length} story!</p>
-      </Row>}
-      
-      <ul>
-        {user.stories?.slice().reverse().map((story) => (
-          <li style={{ listStyle: "none" }} key={story.id}>
-            <StoryComponent story={story} />
-          </li>
-        ))}
-      </ul>
-    </div>
+
+            {isAuthor ? (
+              <></>
+            ) : (
+              <div className="flex justify-center mt-6">
+                <FollowButton followers={user.followers} id={user.id} />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="w-full bg-slate-50 rounded-3xl text-center py-8">
+          <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-black md:text-5xl lg:text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              {user.name}'s
+            </span>{" "}
+            Stories
+          </h1>
+
+          {user.stories && (
+            <h1 className="flex items-center justify-center text-5xl font-extrabold dark:text-black">
+              {user.name}&nbsp; <p className="text-2xl text-gray-600">has</p>
+              <span className="bg-blue-100 text-blue-800 text-2xl font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">
+                {user.stories.length}{" "}
+                {user.stories.length === 1 ? "story" : "stories"}
+              </span>
+            </h1>
+          )}
+
+          <div className=" border-solid rounded-2xl  flex flex-wrap justify-center">
+            {user.stories
+              ?.slice()
+              .reverse()
+              .map((story) => (
+                <div key={story.id} className="p-2">
+                  <StoryComponent story={story} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
