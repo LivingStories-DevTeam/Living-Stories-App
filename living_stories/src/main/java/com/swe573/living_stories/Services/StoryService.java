caab944c678bc1b29d18408318c278b528d96717
Ali Hakan <ozen.alihakan@gmail.com>
2023-12-17 15:54:06 +0300

    Updating Legacy Story Creation Service for Polygon Creation #149

--- living_stories/src/main/java/com/swe573/living_stories/Services/StoryService.java
+++ living_stories/src/main/java/com/swe573/living_stories/Services/StoryService.java
@@ -1,6 +1,6 @@
 package com.swe573.living_stories.Services;
 
-import com.swe573.living_stories.Confrugation.DateParser;
+import com.swe573.living_stories.Configuration.DateParser;
 import com.swe573.living_stories.DTO.MediaDTO;
 import com.swe573.living_stories.Models.*;
 import com.swe573.living_stories.Repositories.StoryRepository;
