d6c7dbb9615c1876cf387778b82bb7fb992e42d4
yavuzsa <slhyvz7@gmail.com>
2024-01-02 17:58:05 +0300

    Add unit test for service methods

--- living_stories/src/test/java/com/swe573/living_stories/Services/UserServiceTest.java
+++ living_stories/src/test/java/com/swe573/living_stories/Services/UserServiceTest.java
@@ -331,6 +331,37 @@ class UserServiceTest {
         verify(userRepository, times(1)).findByNameContainingIgnoreCase(name);
     }
 
+    @Test
+    void testDeleteUserById_UserExists() {
+        // Arrange
+        Long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(new User()));
+
+        // Act
+        boolean result = userService.deleteUserById(userId);
+
+        // Assert
+        assertTrue(result);
+        verify(userRepository, times(1)).findById(userId);
+        verify(userRepository, times(1)).deleteById(userId);
+    }
+
+    @Test
+    void testDeleteUserById_UserNotExists() {
+        // Arrange
+        Long userId = 1L;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // Act
+        boolean result = userService.deleteUserById(userId);
+
+        // Assert
+        assertFalse(result);
+        verify(userRepository, times(1)).findById(userId);
+        verify(userRepository, times(0)).deleteById(userId);
+    }
 
 
 }
